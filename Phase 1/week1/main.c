81ad1745ddb248a99ba5ce4397482802af23fee9
“Aaron <aarondanielmolina@gmail.com>
2026-07-30 13:30:21 -0400

    completed

--- Phase 1/week1/main.c
+++ Phase 1/week1/main.c
@@ -23,8 +23,8 @@ int main()
 
       ////stats////
     //health
-    int p1hp = 2000;
-    int p2hp = 2000;
+    int p1hp = 700;
+    int p2hp = 400;
         
     //attack//
     int p1atk = 550;
@@ -37,44 +37,48 @@ int main()
     int p1crit = 10;
     int p2crit = 15;
 
-    int heroStats[]    = {p1hp, p1atk, p1def, p1crit};
-    int villainStats[] = {p2hp, p1atk, p2def, p2crit};
+        printf("//-----------THE BATTLE BEGINS-----------//\n");
 
     for (int i = 1; i < 6; i++)
     {
+
+        if(p1hp <= 0 || p2hp <= 0) break;
+
+        printf("-------------------------------------------\n");
+        printf("------------------TURN %i------------------\n",i);
+        printf("-------------------------------------------\n");
+        printf("     Hero HP: %i    |    Villian HP: %i   \n",p1hp,p2hp);
+        printf("-------------------------------------------\n");
+        
 
         int base_dmg;
         int var_damage;
         float element_dmg;
         int attacker_crit;
-        int defender_hp;
+        char* attacker;
+        char* defender;
 
-        if (p1hp <= 0 || p2hp <= 0)
-        {
-            break;
-        }
 
         if (i % 2 != 0)
         {
             base_dmg = roll_base_damage(p1atk, p2def);
             attacker_crit = p1crit;
-            defender_hp = p2hp;
+            attacker = person1;
+            defender = person2;
+            printf("Hero gets ready to attack!\n");
         }
         else
         {
             base_dmg = roll_base_damage(p2atk, p1def);
             attacker_crit = p2crit;
-            defender_hp = p1hp;
+            attacker = person2;
+            defender = person1;
+            printf("Villian gets ready to attack!\n");
         }
                    
         var_damage = apply_variance(base_dmg);
         element_dmg = elemental_multiplier();
 
-        if (is_critical(attacker_crit))
-        {
-            var_damage = var_damage * 2;
-        }
-            
         if (element_dmg == 2.0)
         {
             var_damage = var_damage + 100;
@@ -86,13 +90,41 @@ int main()
             printf("Elemental disadvantage! Damage reduced.\n");
         }
 
+        if (is_critical(attacker_crit))
+        {
+            var_damage = var_damage * 2;
+            printf("It was a critical attack!\n");
+        }
+            
+
+
         if(var_damage <= 0) var_damage = 1;
 
-        printf("attack is %d \n",var_damage);
-        printf("/////------//////");
+        if (i % 2 != 0)
+        {
+            p2hp -= var_damage;
+        }
+        else
+        {
+            p1hp -= var_damage;
+        }
+
+        printf("%s attacks %s for %i\n",attacker, defender, var_damage);
         
+        if(p1hp <= 0)
+        {
+            printf("The Hero is defeated!\n");
+        }
+        else if (p2hp <= 0)
+        {
+            printf("The Villian is defeated!\n");
+        }
         
     } 
+
+
+
+    printf("-------------------------------------------\n");
 
 }
 
@@ -117,7 +149,7 @@ int apply_variance(int damage)
     printf("Percent modifier is %d\n", percent);
     
     float percent_of_damage = (damage * percent) / 100;
-    //printf("%d percent of %i is %f\n", percent, damage, percent_of_damage);
+    printf("%d percent of %i is %f\n", percent, damage, percent_of_damage);
 
     int flip_num = rand() % 100;
 
@@ -158,7 +190,6 @@ int is_critical(int crit_chance_percent)
 float elemental_multiplier()
 {
     float didItHit = 1.0;
-    //char* element[] = {"earth","air","water","fire"};
     int atk_el = (rand() % 4) + 1;
     int def_el = (rand() % 4) + 1;
 
