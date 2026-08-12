f91e53d85f0519989b81354def150e03a0ed32cd
“Aaron <aarondanielmolina@gmail.com>
2026-08-11 21:48:59 -0400

    adding remove function

--- Phase 1/week3_a/main.c
+++ Phase 1/week3_a/main.c
@@ -1,47 +1,58 @@
 #include <stdio.h> 
+#include <string.h>
 
-int inv_add(char names[][32], int *count, const char *name); //appends(adds) if room
-int inv_remove(char names[][32], int *count, int index); //removes at index by shifting later items down
-void inv_list(char *name[], int count); //prints item list
-int check_index(char* index); //check if index is empty
+void inv_add(char names[][32], int *count, const char *name); //appends(adds) if room
+void inv_remove(char names[][32], int *count, int index); //removes at index by shifting later items down
+void inv_list(char names[][32], int count); //prints item list
+
 
 int main(void)
 {
-    char* inventory[10] = {"Potion", "Gold Coin"};
-    inv_list(inventory, 10);
-
-    char* input = "";
-
-    do
-    {
-        inv_list(inventory, 10);
-    } while (input != "q" || input != "Q" );
+    int count = 2;
+    char inventory[10][32] = {
+        {'P','o','t','i','o','n'},
+        {'E','l','i','x','i','r'}
+    };
     
     
+    inv_list(inventory, count);
+    inv_add(inventory, &count, "Poison");
+    inv_list(inventory, count);
+    inv_remove(inventory, &count, 1);
+
+
+
+
 
 }
 
-int check_index(char* index)
-{
-    //returns 1 if empty, return 0 if not
-    int isEmpty = 1;
-    if (index != NULL) 
-    {
-        isEmpty = 0;
-        //printf("Inventory slot is full\n");
-    }
-    else
-    {
-        //printf("Inventory slot is available\n");
-    }
 
-    return isEmpty;
-}
 
-void inv_list(char *name[], int count)
+void inv_list(char names[][32], int count)
 {
+    printf("Item Count: %d\n", count);
     for(int i = 0; i < count; i++)
     {
-        if(name[i] != NULL) printf("%s\n", name[i]);
+        if(names[i][0] != '\0') printf("%s\n", names[i]);
+        //else printf("Empty Slot\n");
     }
+}
+
+void inv_add(char names[][32], int *count, const char *name)
+{
+    int length = strlen(name);
+
+    int spot = *count;
+    for(int i = 0; i < length; i++)
+    {
+        names[spot][i] = name[i];
+    }
+        
+    *count += 1;
+}   
+
+void inv_remove(char names[][32], int *count, int index)
+{
+    int dif =  
+    for(int i = 0; i )
 }
