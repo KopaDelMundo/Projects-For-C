2b706749cf66ac1b3afa4139d0359983e36dfa4d
“Aaron <aarondanielmolina@gmail.com>
2026-08-13 15:18:36 -0400

    working on loop menu

--- Phase 1/week3_a/main.c
+++ Phase 1/week3_a/main.c
@@ -1,10 +1,14 @@
+//TODO: Wrap all functions into a loop that takes inputß
+
 #include <stdio.h> 
 #include <string.h>
 
 void inv_add(char names[][32], int *count, const char *name); //appends(adds) if room
 void inv_remove(char names[][32], int *count, int index); //removes at index by shifting later items down
 void inv_list(char names[][32], int count); //prints item list
-
+int read_menu_choice(void);
+void handle_input(int choice);
+char* handle_string(void);
 
 int main(void)
 {
@@ -14,19 +18,59 @@ int main(void)
         {'E','l','i','x','i','r'}
     };
     
+    int choice = 0;
     
-    inv_list(inventory, count);
-    inv_add(inventory, &count, "Poison");
-    inv_list(inventory, count);
-    inv_remove(inventory, &count, 1);
 
+    do
+    {
+        inv_list(inventory, count);
+        choice = read_menu_choice();
+        void handle_input(choice);
 
-
-
+    } while (choice != 3);
+    
 
 }
 
+char* handle_string(void)
+{
+    char item[32] = "";
+    int input = scanf("%s", item);
+    
+    return item;
+}
 
+void handle_input(int choice)
+{
+    switch (choice)
+    {
+        case 1:
+
+
+    }
+}
+
+int read_menu_choice(void)
+{
+    int input = 0;
+    int choice = 0;
+    do
+    {
+        printf("Enter 1 to add an item, 2 to remove an item, or 3 to quit.\n");
+        input = scanf("%i", &choice);
+
+        if(input != 1)
+        {
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF){ }
+            choice = 0;
+        }
+        
+
+    } while (choice < 1 || choice > 3);
+    
+    return choice;
+}
 
 void inv_list(char names[][32], int count)
 {
@@ -53,13 +97,14 @@ void inv_add(char names[][32], int *count, const char *name)
 
 void inv_remove(char names[][32], int *count, int index)
 {
-    for (int i = index; i < count - 1; i++)
+    int amount = *count - 1;
+    for (int i = index; i < amount; i++)
     {
-        if(i != count -1)
+        if(i != amount)
         {
-            names[i][31] = names[i+1][31];
+            strcpy(names[i], names[i+1]);
         }
-
-        count -= 1;
     }
+
+    *count -= 1;
 }
