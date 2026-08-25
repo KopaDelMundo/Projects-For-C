8a0046885b035f348f1c7b543702c7e4d3e40d61
“Aaron <aarondanielmolina@gmail.com>
2026-08-25 16:57:54 -0400

    working on cmd parser

--- Phase 1/week5/main.c
+++ Phase 1/week5/main.c
@@ -1,37 +1,50 @@
 #include <stdio.h>
 
-
-void log_hit(const char* attacker, const char* defender, int dmg);
+void log_hit(const char* attacker, const char* defender, int dmg, int is_crit, int is_miss);
 int parse_command(const char* line, char*verb_out, char *arg_out);
-void note_trunc(int a, int b);
+void note_trunc(int written, int buffer_size);
+
 
 int main(void)
 {
-    log_hit("Aaron", "Slime", 200);
-    log_hit("Slime", "Aaron", 0);
+    //log_hit("Hubert Blaine Wolfeschlegelsteinhausenbergerdorff", "Slime", 200, 1, 0);
+    log_hit("Aaron", "Slime", 200, 1, 0);
+    log_hit("Slime", "Aaron", 100, 1, 1);
 }
 
-void log_hit(const char* attacker, const char* defender, int dmg)
+int parse_command(const char* line, char*verb_out, char *arg_out)
 {
-    char buf[128];
-    int buf_size = (int)sizeof(buf);
-    int n;
+    int tokens = 0;//spaces indicate # of tokens
+    char* cmds[] = {"attack", "use", "look", "quit"}; //valid commands
 
-    if(dmg == 0) //0 dmg assumes a miss for now
+    
+
+}
+
+void log_hit(const char* attacker, const char* defender, int dmg, int is_crit, int is_miss)
+{
+    char buf[30];
+    int buf_size = (int)sizeof(buf);
+    int n = 0;
+
+    if(is_miss) //0 dmg assumes a miss for now
     {
         n = snprintf(buf, sizeof(buf), "%s's attack missed!", attacker);
-        note_trunc(n, buf_size);
+    }
+    else if(is_crit)
+    {
+        n = snprintf(buf, sizeof(buf), "%s attacks %s! It was critical! %d damage!", attacker, defender, dmg);
     }
     else
     {
-        n = snprintf(buf, sizeof(buf), "%s attacks %s! %d damage!", attacker, defender, dmg);
-        note_trunc(n, buf_size);
+        n = snprintf(buf, sizeof(buf), "%s attacks %s! %d damage!", attacker, defender, dmg); 
     }
 
+    note_trunc(n, buf_size);
     printf("%s\n", buf);
 }
 
-void note_trunc(int a, int b)
+void note_trunc(int written, int buffer_size)
 {
-    if(a >= b) fprintf(stderr, "warning: log message truncated\n");
+    if(written >= buffer_size) fprintf(stderr, "warning: log message truncated\n");
 }
