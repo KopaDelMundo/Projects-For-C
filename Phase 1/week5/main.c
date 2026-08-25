fc65b19c553dfc02b395e731ba8dd0d9481dc5ff
“Aaron <aarondanielmolina@gmail.com>
2026-08-25 13:06:02 -0400

    starting week 5 work

---
+++ Phase 1/week5/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void log_hit(const char* attacker, const char* defender, int dmg);
+int parse_command(const char* line, char*verb_out, char *arg_out);
+
+int main(void)
+{
+
+}
+
+void log_hit(const char* attacker, const char* defender, int dmg)
+{
+    if(dmg != 0) //0 dmg assumes a miss for now
+    {
+        
+    }
+    else
+    {
+
+    }
+}
