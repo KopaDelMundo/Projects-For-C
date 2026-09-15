78fccc0b060aff7323171956b5f646b7bb41fd50
Aaron <106047608+KopaDelMundo@users.noreply.github.com>
2026-09-15 13:38:41 -0400

    functions mostly complete, need feedback and to check address sanitizer

--- Phase 1/week7/main.c
+++ Phase 1/week7/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BASE_CAP 10;
+#define BASE_CAP 10
 
 //TODO: 
 
@@ -27,31 +27,57 @@ int main(void)
 {
     DynArray dy1;
     da_init(&dy1);
-    Entity e1 = { 1 };
-    Entity e2 = { 2 };
-    Entity e3 = { 3 };
-    Entity e4 = { 4 };
-    Entity e5 = { 5 };
+    Entity e1 =     { 1 };
+    Entity e2 =     { 2 };
+    Entity e3 =     { 3 };
+    Entity e4 =     { 4 };
+    Entity e5 =     { 5 };
+    Entity e6 =     { 6 };
+    Entity e7 =     { 7 };
+    Entity e8 =     { 8 };
+    Entity e9 =     { 9 };
+    Entity e10 =    { 10 };
+    Entity e11 =    { 11 };
+    Entity e12 =    { 12 };
+    Entity e13 =    { 13 };
     da_push(&dy1, &e1);
     da_push(&dy1, &e2);
     da_push(&dy1, &e3);
     da_push(&dy1, &e4);
     da_push(&dy1, &e5);
-
+    da_push(&dy1, &e6);
+    da_push(&dy1, &e7);
+    da_push(&dy1, &e8);
+    da_push(&dy1, &e9);
     
-
-    //Entity *p = da_get(&dy1, 0);
 
     print_DynArray(&dy1);
 
+    printf("----------------------------------------\n");
+    printf("----------------------------------------\n");
+
+    da_push(&dy1, &e10);
+    da_push(&dy1, &e11);
+    da_push(&dy1, &e12);
+    da_push(&dy1, &e13);
+
+    print_DynArray(&dy1);
+
+    da_remove_swap(&dy1, 12);
+    da_remove_swap(&dy1, 7);
     da_remove_swap(&dy1, 0);
+    
+
     printf("----------------------------------------\n");
     printf("----------------------------------------\n");
     
+    print_DynArray(&dy1);
+
+    da_free(&dy1);
+
     print_DynArray(&dy1);
 
     free(dy1.items);
-    
 }
 
 void da_init(DynArray *a)
@@ -59,20 +85,28 @@ void da_init(DynArray *a)
     a->capacity = 10;
     a->count = 0;
     a->items = malloc(sizeof(Entity) * 10);
-    printf("|DEBUG| Dynamic array initialized.\n");
+    printf("Dynamic array initialized.\n");
 }
 
 void da_push(DynArray *a, Entity *e)
 {
+    Entity *tmp;
+
     if(a->count + 1 >= a->capacity)
     {
-        a->capacity *= 2;
-        a->items = realloc(a->items, sizeof(Entity) * a->capacity);
+        a->capacity = a->capacity * 2;
+        
+        tmp = realloc(a->items, sizeof(Entity) * a->capacity);
+        if(tmp == NULL)
+        {
+            printf("ALLOCATION FAILED: ABORTING\n");
+            return;
+        }
+        a->items = tmp;
+        
     }
-
     a->items[a->count] = *e;
     a->count++;
-
 }
 
 Entity *da_get(DynArray *a, int i)
@@ -92,28 +126,40 @@ Entity *da_get(DynArray *a, int i)
 
 void da_remove_swap(DynArray *a, int num)
 {
+    Entity *tmp;
+    int new_capacity;
 
+    //printf("The bounds are 0 and %d\n", a->count - 1);
+    
     if(num < 0 || num > (a->count - 1))
     {
-        printf("Not a valid selection.\n");
+        printf("%d is not a valid selection.\n", num);
     }
     else
     {
+        printf("Removing Entity at index: %d\n", num);
+
         a->items[num] = a->items[a->count - 1];
-        if(a->count < a->capacity / 2 && a->capacity % 10 == 0)
-        {
-            if(a->capacity < 10)
-            {
-                a->capacity = 10;
-            }
-            else
-            {
-                a->capacity /= 2;
-            }
-            
-            a->items = realloc(a->items, sizeof(Entity) * a->capacity);
-        }
         a->count -= 1;
+        
+
+        if(a->count <= a->capacity / 4 && a->capacity / 2 >= BASE_CAP)
+        {
+
+
+            new_capacity = a->capacity / 2;
+
+            
+            tmp = realloc(a->items, sizeof(Entity) * new_capacity);
+            if(tmp == NULL)
+            {
+                printf("ALLOCATION FAILED: ABORTING\n");
+                return;
+            }
+
+            a->items = tmp;
+            a->capacity = new_capacity;
+        }   
     }
 }
 
@@ -126,10 +172,14 @@ void print_DynArray(DynArray *a)
         printf("Entity: %d \n", a->items[i].itemNum);
         printf("----------------------------------------\n");
     }
+    printf("Current Count: %d\n", a->count);
+    printf("Current Capacity: %d\n", a->capacity);
 }
 
 void da_free(DynArray *a)
 {
+    printf("Clearing out dynamic array...\n");
     a->items[0].itemNum = 0;
     a->capacity = BASE_CAP;
+    a->count = 0;
 }
